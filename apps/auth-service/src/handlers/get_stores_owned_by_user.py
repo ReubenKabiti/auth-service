59325b4e4bd3dfed58cfbc1f4b2934311791632a
ReubenKabiti <reubenrk2@gmail.com>
2024-08-05 01:04:39 +0300

    refactor: move store reading code

--- apps/auth-service/src/handlers/get_stores_owned_by_user.py
+++ apps/auth-service/src/handlers/get_stores_owned_by_user.py
@@ -1,15 +1,11 @@
-import boto3
+from .lib.store import read_stores_from_user
 from .lib.util.returns import return_json
-
-table = boto3.resource("dynamodb").Table("StoresTable")
 
 def handler(event, context):
     user_id = event["pathParameters"]["id"]
 
-    res = table.query(
-        IndexName="userId",
-        KeyConditionExpression=boto3.dynamodb.conditions.Key("user_id").eq(user_id)
-    )
+    res = read_stores_from_user(user_id)
+
     if not "Items" in res:
         return return_json({"message": "user doesn't exist"}, 404)
     items = res["Items"]
--- apps/auth-service/src/handlers/lib/store.py
+++ apps/auth-service/src/handlers/lib/store.py
@@ -6,12 +6,16 @@ def create_store(params):
     pass
 
 def read_store(store_id):
-    response = store_table.get_item(Key={"id": store_id})
+    response = stores_table.get_item(Key={"id": store_id})
     store = response.get("Item")
     return store
 
-def read_stores_from_user(params):
-    pass
+def read_stores_from_user(user_id):
+    res = stores_table.query(
+        IndexName="userId",
+        KeyConditionExpression=boto3.dynamodb.conditions.Key("user_id").eq(user_id)
+    )
+    return res
 
 def update_store(params):
     pass
