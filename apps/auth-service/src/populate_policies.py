c0992d4292a7acbbbcee44f66fb98f4ccb4b0ed0
ReubenKabiti <reubenrk2@gmail.com>
2024-08-15 08:26:38 +0300

    rewrite populate_policies.py

--- apps/auth-service/src/populate_policies.py
+++ apps/auth-service/src/populate_policies.py
@@ -1,5 +1,5 @@
 import boto3
-from handlers.lib.resource_creation import ResourceAction, get_all_resources, create_policy
+from handlers.lib.resource_creation import get_all_policies
 from typing import List
 from uuid import uuid4 as uuid
 
@@ -8,17 +8,13 @@ table = boto3.resource("dynamodb").Table("PoliciesTable")
 with open("./cedar_test_resources.json", "r") as file:
     json_text = file.read()
 
-resource_actions: List[ResourceAction] = get_all_resources(json_text)
+policies = get_all_policies(json_text)
 
-policies = []
-
-for resource_action in resource_actions:
-    policy = create_policy(resource_action)
-
+for policy in policies:
     item = {
         "id": str(uuid()),
-        "policy_name": resource_action.get_policy_name(),
-        "policy_definition": policy,
+        "action_name": policy.action_name,
+        "policy_definition": str(policy),
     }
 
     table.put_item(Item=item)
